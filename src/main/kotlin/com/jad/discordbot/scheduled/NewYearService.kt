f9aca6d36b6d64be3b93ca07d86bc782a2bc0d51
Jan Adamczyk <j_adamczyk@hotmail.com>
2022-12-31 14:44:09 +0100

    year hardcoded, cron at year change to see what happens

--- src/main/kotlin/com/jad/discordbot/scheduled/NewYearService.kt
+++ src/main/kotlin/com/jad/discordbot/scheduled/NewYearService.kt
@@ -20,13 +20,14 @@ class NewYearService(private val botUtils: BotUtils) {
     @Retryable(value = [Exception::class], maxAttempts = 2, backoff = Backoff(delay = 1000))
     fun postNewYearMessage() {
         logger.info("Posting new year message")
+        logger.info("Current year: ${Year.now().value}, last year: ${Year.now().value - 1}")
 
         val botChannel = botUtils.getMainChannel()
 
         val embed: EmbedCreateSpec = EmbedCreateSpec.builder()
-            .title("A wild ${Year.now().value} appears!")
+            .title("A wild 2023 appears!")
             .description(
-                "${Year.now().value - 1} ist vorbei - Zeit für ein neues Jahr!" +
+                "2022 ist vorbei - Zeit für ein neues Jahr!" +
                         "\n Ich hoffe ihr blickt auf ein erfolgreiches und spannendes Jahr zurück und genießt die Zeit mit euren Liebsten." +
                         "\n\n Sogar Ash ist mittlerweile Pokemon Meister - also gibt es im neuen Jahr keine Ausreden. " +
                         "\n Erfüllt euch eure Träume und Wünsche und habt eine gute Zeit dabei :)"
