cb8d8bb09c44aa621c1041d464a143493c4ab7c7
Jan Adamczyk <j_adamczyk@hotmail.com>
2023-04-01 00:07:58 +0200

    eureka discovery

--- src/main/kotlin/com/jad/discordbot/DiscordbotApplication.kt
+++ src/main/kotlin/com/jad/discordbot/DiscordbotApplication.kt
@@ -2,10 +2,12 @@ package com.jad.discordbot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.retry.annotation.EnableRetry
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableRetry
+@EnableDiscoveryClient
 @EnableScheduling
 @SpringBootApplication
 class DiscordbotApplication
